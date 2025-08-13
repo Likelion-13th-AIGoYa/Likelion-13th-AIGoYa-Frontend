1745866698c03c9ff5ba90175ee8ccd2ae2a65c6
Ukja2 <jaewooklee739@gmail.com>
2025-08-13 19:45:06 +0900

    feat: 날씨 컴포넌트 틀 구현

---
+++ aigoya/src/component/mainWeather.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "../css/mainWeather.module.css";
+
+// 더미 데이터
+const dummyData = {
+  temperature: 18,
+  summary: "흐린 후 비",
+  aiMenuSuggestion: "따뜻한 국물 요리 주문량 증가 예상",
+};
+
+
+
+function mainWeather() {
+  const { temperature, summary, aiMenuSuggestion } = dummyData;
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.weatherInfo}>
+        <h2 className={styles.temp}>{temperature}°C</h2>
+        <p className={styles.summary}>{summary}</p>
+      </div>
+      <div className={styles.recommendation}>
+        <p>{aiMenuSuggestion}</p>
+      </div>
+    </div>
+  );
+}
+
+export default mainWeather
